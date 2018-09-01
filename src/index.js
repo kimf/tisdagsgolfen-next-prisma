0ffcd605d4aed8eb74c4820110d59ef1c9d0b1bb
Kim Fransman <kim.fransman@gmail.com>
2018-09-01 17:47:34 +0200

    setup stuff

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const { GraphQLServer } = require("graphql-yoga");
+const { Prisma } = require("prisma-binding");
+
+const resolvers = {
+  Query: {
+    user(parent, args, context, info) {
+      return context.db.query.user({ where: { id: args.userId } }, info);
+    },
+    userByName(parent, args, context, info) {
+      return context.db.query.users(
+        {
+          where: {
+            name: args.name
+          }
+        },
+        info
+      );
+    }
+  },
+  Mutation: {
+    createUser(parent, args, context, info) {
+      return context.db.mutation.createUser(
+        {
+          data: {
+            name: args.name
+          }
+        },
+        info
+      );
+    }
+  }
+};
+
+// author: {
+//   connect: { id: args.userId }
+// }
+
+const server = new GraphQLServer({
+  typeDefs: "./schema.graphql",
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: "prisma.graphql",
+      endpoint: "https://eu1.prisma.sh/kim-fransman-4c718b/tisdagsgolfen/dev"
+    })
+  })
+});
+server.start(() => console.log("Server is running on http://localhost:4000"));
